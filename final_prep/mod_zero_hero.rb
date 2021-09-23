9fe64e3283f6ab17a869047f94dd85ccac0e96e5
Eric Chon <echon92@gmail.com>
2021-09-23 14:32:14 -0600

    Completed hero final prep

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -1,45 +1,70 @@
 # Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 # Declare two variables - hero_name AND special_ability - set to strings
+hero_name = "ZeroHero"
+special_ability = "Flying"
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
-
+greeting = "Hello I am #{hero_name}"
+catchphrase = "super #{special_ability} kick"
 # Declare two variables - power AND energy - set to integers
+
+power = 9001
+energy = 12
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
+full_power = power * 500
+full_energy = energy + 150
 
 # Declare two variables - is_human and identity_concealed - assigned to booleans
-
+is_human = true
+identity_concealed = false
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
+arch_enemies = ["Negative1", "Negative2", "Negative3"]
+sidekicks = ["Number1", "Number2", "Number3"]
 
 # Print the first sidekick to your terminal
-
+p sidekicks[0]
 # Print the last arch_enemy to the terminal
-
+p arch_enemies[2]
 # Write some code to add a new arch_enemy to the arch_enemies array
-
+arch_enemies << "Negative4"
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
-
+p arch_enemies
 # Remove the first sidekick from the sidekicks array
-
+sidekicks.shift
 # Print the sidekicks array to terminal to ensure you added a new sidekick
-
+p sidekicks
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+def assess_situation(danger_level, save_the_day, bad_excuse)
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level >= 10 && danger_level <= 50
+    puts save_the_day
+  else danger_level < 10
+    puts "Meh. Hard pass"
+  end
+end
+
+p assess_situation(5, "We did it!", "I forgot I left my stove on, be right back.")
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
+
+
+
 
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -55,17 +80,29 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
-
+scary_monster = {
+  'name' => "Chappy",
+  'smell' => "onions",
+  'weight' => 150,
+  'citiesDestroyed' => ["Denver", "Aurora", "Parker"],
+  'luckyNumbers' => [3, 7, 4],
+  'address' => {
+    'number' => 3422,
+    'street' => "Wall",
+    'state' => "Colorado",
+    'zip' => "80016"
+  }
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
@@ -73,12 +110,43 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
 
 # - Create 2 instances of your SuperHero class
+class SuperHero
+  attr_accessor :name, :super_power, :age, :arch_nemesis, :power_level, :energy_level
+
+  def initialize(name, super_power, age)
+    @name = name
+    @super_power = super_power
+    @age = age
+    @arch_nemesis = "The Syntax Error"
+    @power_level = 100
+    @energy_level = 50
+  end
+
+ def say_name
+   puts "Hello, I am #{@name}"
+ end
+
+ def maximize_energy
+   energy_level = 1000
+ end
+
+ def gain_power(multiplier)
+   power_level + multiplier
+ end
+end
+
+invincible = SuperHero.new('Invincible', "Really strong", 21)
+beeman = SuperHero.new('Beeman', "Controls bees", 24)
+
+p invincible.say_name
+p invincible.arch_nemesis
+
 
 
 # Reflection
 # What parts were most difficult about this exerise?
-
+# Setting up a static value for the class.
 # What parts felt most comfortable to you?
-
+# Setting up dynamic value for the class.
 # What skills do you need to continue to practice before starting Mod 1?
-
+# vocabulary and classes. 
